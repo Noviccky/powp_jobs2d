c946735d4bfb05fa11ed11ede729aed3ab2bd52e
Noviccky <mateusz.nowicki112@gmail.com>
2025-12-27 16:15:37 +0100

    Command model support implemented, ComplexCommand added

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.jobs2d.commands;
+
+import java.util.List;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.DriverCommand;
+
+public class ComplexCommand implements DriverCommand {
+    private List<DriverCommand> commands;
+
+    public ComplexCommand(List<DriverCommand> commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        for (DriverCommand command: commands) {
+            command.execute(driver);
+        }
+    }
+}
